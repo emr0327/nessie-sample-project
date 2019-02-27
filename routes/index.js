24410de2e5e0cb8913d5767cd9333af785ef5b69
Elisa-Michelle Rodriguez <sdq217@capitalone.com>
2019-02-27 15:25:08 -0500

    Added accounts show route

--- routes/index.js
+++ routes/index.js
@@ -15,24 +15,67 @@ var options = {
   json: true // Automatically parses the JSON string in the response
 };
 
-/* GET home page. */
+var customers = {
+  uri: BASE_URL + '/customers/',
+  qs: {
+    key: API_KEY_PARAM // -> uri + '?access_token=xxxxx%20xxxxx'
+  },
+  headers: {
+    'User-Agent': 'Request-Promise-Native'
+  },
+  json: true // Automatically parses the JSON string in the response
+}
+
+var accounts = {
+  uri: BASE_URL + '/customers',
+  qs: {
+    key: API_KEY_PARAM // -> uri + '?access_token=xxxxx%20xxxxx'
+  },
+  headers: {
+    'User-Agent': 'Request-Promise-Native'
+  },
+  json: true // Automatically parses the JSON string in the response
+}
+
+// GET home page.
 router.get('/', function(req, res, next) {
   // res.render('index');
   rp(options)
     .then(customers => {
+      console.log(typeof(customers))
       res.render('index', {
         customers
       });
     })
     .catch(err => {
-      // API call failed...
+      // API call failed
+      res.send(err)
+    });
+});
+
+// SHOW ROUTE
+router.get("/:id", (req, res) => {
+  // res.send('SHOW ME THE MONEY');
+  rp({
+    "method": "GET",
+    "uri": BASE_URL + '/customers/' + req.params.id + '/accounts/',
+    "qs": {
+      key: API_KEY_PARAM
+    },
+    "headers": {
+      'User-Agent': 'Request-Promise-Native'
+    },
+    "json": true
+    })
+    .then(accounts => {
+      res.render('show', {
+        accounts
+      });
+    })
+    .catch(err => {
+      // API call failed
+      res.send(err)
     });
 });
 
 module.exports = router;
-
-// var request = require('superagent');
-// request.get('http://api.reimaginebanking.com/?key=your_key').end(function (res) {
-//   foo(res.status);
-//   bar(res.body); //do something
-// });
