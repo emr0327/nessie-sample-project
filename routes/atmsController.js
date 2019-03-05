ac2b1f8c6a04dfa14f30c4f3e111ca09253bd193
Elisa-Michelle Rodriguez <sdq217@capitalone.com>
2019-03-05 13:58:12 -0500

    Seeded the DB data and created the initial atmsController route

---
+++ routes/atmsController.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var router = express.Router();
+var rp = require("request-promise-native");
+
+var data = require("../models/geo_data").seededGeoData;
+const { BASE_URL, API_KEY_PARAM } = require('../config');
+
+
+var options = {
+  uri: BASE_URL + '/customers',
+  qs: {
+    key: API_KEY_PARAM // -> uri + '?access_token=xxxxx%20xxxxx'
+  },
+  headers: {
+    'User-Agent': 'Request-Promise-Native'
+  },
+  json: true // Automatically parses the JSON string in the response
+};
+
+// INDEX ROUTE
+router.get('/', (req, res, next) => {
+  res.send("List of ATMs soon to come");
+});
+
+module.exports = router;
